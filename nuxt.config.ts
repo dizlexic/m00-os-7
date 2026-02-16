17d165c122e534f9fa974642b5f420c3b420766f
Daniel Moore <dan@mooresolutions.io>
2026-02-15 22:52:06 -0600

    Adds new meta tags for format detection and theme color in the Nuxt configuration.
    Signed-off-by: Daniel Moore <dan@mooresolutions.io>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,9 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'A faithful recreation of Apple Macintosh System 7 as a web application' }
+        { name: 'description', content: 'A faithful recreation of Apple Macintosh System 7 as a web application' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'theme-color', content: '#CCCCCC' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
